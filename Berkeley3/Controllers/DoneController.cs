bca51df9a1ca02a4ec0adba9349f5aef84881e05
Vaughan,Matthew <VAUGHAM2@airproducts.com>
2023-05-10 06:14:01 +0100

    Done

---
+++ Berkeley3/Controllers/DoneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Berkeley3.Controllers
+{
+    public class DoneController : Controller
+    {
+        // GET: Done
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Berkeley3/Controllers/SignUpController.cs
+++ Berkeley3/Controllers/SignUpController.cs
@@ -15,17 +15,22 @@ namespace Berkeley3.Controllers
         [HttpPost]
         public ActionResult Index(SignUpDetails signUpDetails)
         {
+            var isValid = true;
             // Fluent Validator separates concerns.
             SignUpDetailsValidator validator = new SignUpDetailsValidator();
             ValidationResult result = validator.Validate(signUpDetails);
             if (!result.IsValid)
             {
+                isValid = false;
                 foreach (ValidationFailure failer in result.Errors)
                 {
                     ModelState.AddModelError(failer.PropertyName, failer.ErrorMessage);
                 }
             }
-            return View(signUpDetails);
+            if (isValid)
+                return RedirectToRoute(new { controller = "Done", action = "Index" });
+            else
+                return View(signUpDetails);
         }
     }
 }
